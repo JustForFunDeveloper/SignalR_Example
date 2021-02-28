5701cbb5ecb5ea2966cea3b9748e88734bfe6869
JustForFunDeveloper <tappler.andreas@gmail.com>
2021-02-28 11:58:23 +0100

    Add project files.

---
+++ SignalR_Example/Hub/SignalRHub.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace SignalR_Example.Hub
+{
+    public class SignalRHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        private ILogger<SignalRHub> _logger;
+
+        public SignalRHub(ILogger<SignalRHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InitData(string testString)
+        {
+            _logger.LogInformation(testString);
+            var data = new
+            {
+                TestStringAnswer = "??",
+                TestInt = 42
+            };
+            // Using the Newtonsoft.Json NuGet package to serialize the object to a json string
+            await Clients.All.SendAsync("TestClientCall", JsonConvert.SerializeObject(data));
+        }
+    }
+}
